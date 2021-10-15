16ae1cfdcb03e6c83d02ca15e130eefe2e772ce5
ZaneFadul <theboonandthezane@gmail.com>
2021-10-15 09:18:02 -0400

    separated AudioManager and test into a separate file

--- scenes/audio/AudioManager/song_params.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-var params = {
-	'on_end' : null,
-	'other_callback' : null,
-	'beats_to_emit_signal': null,
-	'next_song' : null,
-	'next_song_params' : null
-}
--- scenes/level/test.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-func _process(_delta):
-	if Input.is_action_pressed('ui_down'):
-		$globals.index = 2
-	if Input.is_action_pressed('ui_up'):
-		$globals.index = 1
-	if Input.is_action_pressed('ui_left'):
-		$globals.index = 0
